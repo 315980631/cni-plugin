c78487def057f6b6b40c33bf056de37dee3ba90a
liabio <837448792@qq.com>
2020-04-21 09:09:26 +0800

    第二次提交改动代码. 加按服务管理IP，ServiceIPPool资源结构体

---
+++ pkg/k8s/apis/serviceippool/v1alpha1/types.go
@@ -0,0 +1,45 @@
+// Copyright (c) 2019 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ServiceIPPool contains the specification for an relative between service and ippools.
+type ServiceIPPool struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	// Specification of the IPAMBlock.
+	Spec ServiceIPPoolSpec `json:"spec,omitempty"`
+}
+
+// ServiceIPPoolSpec contains the enabled and disabled ippools
+type ServiceIPPoolSpec struct {
+	IPPoolList         []string `json:"ippoolList"`
+	DisabledIPPoolList []string `json:"disabledIPPoolList"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ServiceIPPoolList contains a list of ServiceIPPool resources.
+type ServiceIPPoolList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []ServiceIPPool `json:"items"`
+}
