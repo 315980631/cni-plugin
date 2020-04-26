61a340f95f68697ab8fe6f349c3df58e4ed841b9
liabio <837448792@qq.com>
2020-04-26 22:38:20 +0800

    更新deepcopy

--- build-images.sh
+++ build-images.sh
@@ -1,1 +1,2 @@
-docker build -t calico-cni:v3.12.0-fix --build-arg GIT_VERSION=1.8.3.1 -f Dockerfile . 
+make all
+docker build -t calico/cni:v3.12.0-fix --build-arg GIT_VERSION=1.8.3.1 -f Dockerfile .
